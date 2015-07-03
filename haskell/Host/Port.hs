35aba21beea963a2786a19df3c6bdb190e7615d7
Jacob <leach.jacob@gmail.com>
2015-07-03 16:38:22 -0400

    Adding the new Port class
    Implemented super simple one that just outputs to the local terminal

---
+++ haskell/Host/Port.hs
@@ -0,0 +1,30 @@
+{-|
+Module      : Host.Post
+Description : An interface between pure computer state and impure peripherals
+
+A port will define how the pure computer talks to the impure peripheral. The
+hope is that these will be able to be reused for multiple peripherals with the
+port just defining what sort of IO has to be done to get the message from the
+pure computer to the peripheral which could just be a local terminal or file or
+a remote website.
+
+Hopefully there will be one Websockets Port implementation that can be used by
+numerous different devices.
+-}
+module Host.Port
+  (
+    Port ( sendByte, receiveByte )
+  , LocalTerminal ( LocalTerminal )
+  ) where
+
+import Host.Device(Short, Byte)
+
+class Port a where
+  sendByte :: Short -> a -> Byte -> IO ()
+  receiveByte :: a -> IO (Byte)
+
+data LocalTerminal = LocalTerminal
+
+instance Port LocalTerminal where
+  sendByte address _ value = putStrLn (show value)
+  receiveByte _ = readLn
